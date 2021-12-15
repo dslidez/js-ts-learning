4ec5d78c65642f84c7adbaa4541d51b1a61bbaf3
Daniil <danya.zinovenko@mail.ru>
2021-12-15 22:42:53 +0300

    first commit

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,56 @@
+import { mult, splitIntoWords, sum } from "./01";
+
+
+test('sum should be correct', () => {
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+
+    //action
+    const result1 = sum(a,b);
+    const result2 = sum(b,c);
+
+    //expect result
+    expect(result1).toBe(3);
+    expect(result2).toBe(5);
+
+})
+
+test('multiply should be correct', () => {
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+
+    //action
+    const result1 = mult(a,b);
+    const result2 = mult(b,c);
+
+    //expect result
+    expect(result1).toBe(2);
+    expect(result2).toBe(6);
+
+})
+
+test('splitting into words should be correct', () => {
+    //data
+    const sent1= "Hello my friend!"
+    const sent2= "JS- the best   programming languege"
+
+
+    //action
+    const result1 = splitIntoWords(sent1);
+    const result2 = splitIntoWords(sent2);
+ 
+    //expect result
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe("hello");
+    expect(result1[1]).toBe("my");
+    expect(result1[2]).toBe("friend");
+
+})
+
+
---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,13 @@
+const sentense = "Hello my friends!"
+
+export function splitIntoWords(sentense: string) {
+    return ["hello", "my", "friend"]
+}
+
+export function sum(a: number, b:number) {
+    return a + b
+}
+
+export function mult(a: number, b:number) {
+    return a * b
+}
