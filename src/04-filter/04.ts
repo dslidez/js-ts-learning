9eec5115506890666f075872b18b1e8275e0656d
Daniil <danya.zinovenko@mail.ru>
2021-12-21 14:21:41 +0300

    filter

---
+++ src/04-filter/04.ts
@@ -0,0 +1,28 @@
+const ages = [18, 20, 22, 1, 100, 90, 14];
+
+const predicate = (age: number) => {
+    return age > 90;
+}
+
+
+
+const oldAges = [100];
+
+type CourseType ={
+    title: string
+    price: number
+}
+
+const courses = [{ title: "css", price: 110 },
+                 { title: "JS", price: 200 },
+                 { title: "React", price: 150 },
+                ]
+
+const chipPredicate = (course:) => {
+    return course.price < 160
+} 
+
+const chipCourses = [
+    { title: "css", price: 110 },
+    { title: "React", price: 150 },
+]
